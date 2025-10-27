3cd8a9f355ec38c4f6eb13066f1d3267a5df7262
Meekal Sualeh <meekalshroudy@gmail.com>
2025-10-27 16:21:35 +0500

    added Auth route

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const userRoute = require("./src/routes/user.routes");
 const captainRoute = require("./src/routes/captain.routes");
 const mapRoute = require("./src/routes/map.routes");
 const rideRoute = require("./src/routes/ride.routes")
+const authRoute = require("./src/routes/authorization.routes")
 
 const { connectToMongo } = require("./src/config/db.config")
 connectToMongo();
@@ -26,6 +27,7 @@ app.use("/user", userRoute);
 app.use("/captain", captainRoute);
 app.use("/map", mapRoute);
 app.use("/ride", rideRoute);
+app.use("/auth", authRoute);
 
 module.exports = app
 
