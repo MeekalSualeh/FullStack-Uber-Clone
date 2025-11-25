57d8f5aabd842016282f16801f4575b57e2df065
Meekal Sualeh <meekalshroudy@gmail.com>
2025-11-25 07:14:10 +0500

    made some changes for CORS again

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://full-stack-uber-clone-mu.vercel.app/",
+    origin: "https://full-stack-uber-clone-mu.vercel.app",
     credentials: true
 }))
 
