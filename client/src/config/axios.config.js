e63f004d55678d31d2e4af21e15b0281d0361058
Meekal Sualeh <meekalshroudy@gmail.com>
2025-11-28 03:11:46 +0500

    changes for static site deployment

--- client/src/config/axios.config.js
+++ client/src/config/axios.config.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,
+    // baseURL: import.meta.env.VITE_BACKEND_URL,
+    baseURL: "https://fullstack-uber-clone.onrender.com",
     withCredentials: true,  
 })
