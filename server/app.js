07fe574f9b0a8a636177c15ae834fc8eb7edcd00
Meekal Sualeh <meekalshroudy@gmail.com>
2025-11-28 03:42:49 +0500

    cors changed-3

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://fullstack-uber-clone-1.onrender.coms",
+    origin: "https://fullstack-uber-clone-1.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
 }))
